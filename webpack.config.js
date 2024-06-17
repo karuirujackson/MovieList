4945cb3131bcae6e955bd1be169b357d3832f32f
karuirujackson <jkjackaruiru641@gmail.com>
2024-06-17 14:49:31 +0300

    Added HTML plugins, set the entry pt of app as index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+});
+
+
 module.exports = {
     module: {
         rules: [
@@ -10,4 +17,5 @@ module.exports = {
             },
         ],
     },
+    plugins: [htmlPlugin],
 }
