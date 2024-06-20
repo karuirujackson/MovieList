80083e1f3f3a49c664162ee4109e289d976054a8
karuirujackson <jkjackaruiru641@gmail.com>
2024-06-20 17:02:07 +0300

    Added configuration on the .eslintrc.js and webpack.config.js and some logic to the Card.js and List.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import List from './containers/List';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-const App = () => {
-  return <h1> movieList</h1>
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(
+  <div className='container-fluid'>
+    <nav className='navbar sticky-top navbar-light bg-dark'>
+      <h1 className='navbar-brand text-light'>
+        movieList
+      </h1>
+    </nav>
+    <List />
+  </div>
+);
